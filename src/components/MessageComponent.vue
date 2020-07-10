1a696b66548eae6259be7b06c794447640044163
Alex Chi <iskyzh@gmail.com>
2020-07-10 20:27:23 +0800

    initial commit

---
+++ src/components/MessageComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">{{ msg.name }} {{ msg.time }} {{ msg.message }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Message } from "@/models/Message";
+
+@Component
+export default class MessageComponent extends Vue {
+  @Prop() private message!: Message;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <MessageComponent v-for="message in messages" v-bind:key="message.id" :message="message"></MessageComponent>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import MessageComponent from "@/components/MessageComponent";
+import { AllMessages } from "@/Chat";
+import { Message } from "@/models/Message";
+
+@Component({
+  components: {
+    MessageComponent
+  }
+})
+export default class Home extends Vue {
+  private messages: Message[] = AllMessages;
+}
+</script>
