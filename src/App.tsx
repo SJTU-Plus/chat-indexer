879cd38b5d2e992a5dc91e892f6dd9bf3476fc1e
Alex Chi <iskyzh@gmail.com>
2020-07-14 21:23:11 +0800

    add datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { Button, Intent, Spinner } from "@blueprintjs/core";
+import { DatePicker } from "@blueprintjs/datetime";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container-fluid h-100">
+      <div className="row h-100">
+        <div className="col-auto chat-sidebar">
+          <div className="row">
+            <div className="col my-3">
+              <DatePicker
+                className="chat-datepicker"
+                highlightCurrentDay={true}
+                shortcuts={true}
+                showActionsBar={true}
+              // onChange={(newDate) => this.handleChange(newDate)}
+              // value={this.state.selectedDate}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col my-3">
+              <div className="bp3-input-group">
+                <span className="bp3-icon bp3-icon-search"></span>
+                <input type="text" className="bp3-input" placeholder="搜索" />
+                <button className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-arrow-right"></button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col my-3">
+              <p>
+                共 0 条记录
+              <br />
+                <small className="text-muted">全文搜索仅展示最新的 500 条记录</small>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-auto h-100">
+          <div className="h-100 overflow-auto">
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import "bootstrap/dist/css/bootstrap-grid.css";
 
 ReactDOM.render(
   <React.StrictMode>
