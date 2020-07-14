6885e0e9ac278932ee55677b28bec1c1878d98f7
Alex Chi <iskyzh@gmail.com>
2020-07-14 21:41:29 +0800

    ci: add lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 import "bootstrap/dist/css/bootstrap-grid.css";
 
@@ -10,7 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
